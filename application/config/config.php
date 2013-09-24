0e8ca232e2e0c9090389b5b57577ca58386261bd
Jorge Stamatio <jorge.stamatio@gmail.com>
2013-09-24 04:00:22 +0200

    Back to localhost settings

--- application/config/config.php
+++ application/config/config.php
@@ -14,8 +14,8 @@
 | path to your installation.
 |
 */
-//$config['base_url']	= 'http://localhost/repos/js_cms_ci_hmvc/';
-$config['base_url']	= 'http://sagakhan.jswebdev.ch/';
+$config['base_url']	= 'http://localhost/repos/js_cms_ci_hmvc/';
+//$config['base_url']	= 'http://sagakhan.jswebdev.ch/';
 /*
 |--------------------------------------------------------------------------
 | Index File
